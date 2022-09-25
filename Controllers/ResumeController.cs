1372c3182ca65fdcf505f667e658e0e8116417d6
Viththiyakaran <battikaran6@gmail.com>
2022-09-25 12:21:36 +0530

    Applicant create is working

--- Controllers/ResumeController.cs
+++ Controllers/ResumeController.cs
@@ -1,4 +1,5 @@
 ﻿using Master_Detail.Data;
+using Master_Detail.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -17,7 +18,42 @@ namespace Master_Detail.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            List<Applicant> applicants;
+
+            applicants = _db.applicants.ToList();
+
+            return View(applicants);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        { 
+
+            Applicant applicant = new Applicant();
+
+            applicant.Experiences.Add(new Exprience() { ExperienceId = 1 });
+            applicant.Experiences.Add(new Exprience() { ExperienceId = 2 });
+            applicant.Experiences.Add(new Exprience() { ExperienceId = 3 });
+            applicant.Experiences.Add(new Exprience() { ExperienceId = 4 });
+
+            return View(applicant);
+        }
+
+        public IActionResult Create(Applicant applicant)
+        {
+            foreach(Exprience exprience in applicant.Experiences)
+            {
+                if(exprience.CompanyName==null || exprience.CompanyName.Length == 0)
+                {
+                    applicant.Experiences.Remove(exprience);
+                }
+
+
+            }
+
+            _db.applicants.Add(applicant);
+            _db.SaveChanges();
+             return RedirectToAction("Index");
         }
     }
 }
