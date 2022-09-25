efccdd946c58dec98327d5d12ae9f2331a50e00a
Viththiyakaran <battikaran6@gmail.com>
2022-09-25 19:27:39 +0530

    How to add two tables in one view

---
+++ Controllers/TwoTablesController.cs
@@ -0,0 +1,32 @@
+﻿using Master_Detail.Data;
+using Master_Detail.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Master_Detail.Controllers
+{
+    public class TwoTablesController : Controller
+    {
+
+        public readonly AppDbContext _db;
+
+        public TwoTablesController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            TwoTables two = new TwoTables();
+
+            two.applicants = _db.applicants.ToList();
+
+            two.expriences = _db.expriences.ToList();
+
+            return View(two);
+        }
+    }
+}
---
+++ Models/TwoTables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Master_Detail.Models
+{
+    public class TwoTables
+    {
+        public IEnumerable<Applicant>   applicants { get; set; }
+
+        public IEnumerable<Exprience> expriences { get; set; }
+    }
+}
